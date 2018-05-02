00ec88aabcc85583f1b7049e9dddda1c526b64c3
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2018-05-02 10:37:08 +0530

    Update 8.HapMapCollisionExample.java

--- src/main/java/com/javapoint/8.HapMapCollisionExample.java
+++ src/main/java/com/javapoint/8.HapMapCollisionExample.java
@@ -7,11 +7,17 @@ import java.util.*;
 
 Here are steps, which happens, when you call get() method with key object to retrieve corresponding value from hash based collection
 
-a) Key.hashCode() method is used to find the bucket location in backing array. (Remember HashMap is backed by array in Java) Though hashcode() is not used directly, but they are passed to internal hash() function.
+a) Key.hashCode() method is used to find the bucket location in backing array. 
+(Remember HashMap is backed by array in Java) Though hashcode() is not used directly, but they are passed to internal hash() function.
 
-b) In backing array or better known as bucket, key and values are stored in form of a nested class called Entry. If there is only one Entry at bucket location, than value from that entry is returned. Pretty straightforward right?
+b) In backing array or better known as bucket, key and values are stored in form of a nested class called Entry.
+If there is only one Entry at bucket location, than value from that entry is returned. Pretty straightforward right?
 
-Things get little tricky, when Interviewer ask second question, What happens if two keys has same hashCode? If multiple keys has same hashCode, than during put() operation collision had occurred, which means multiple Entry object stored in a bucket location. Each Entry keep track of another Entry, forming a linked list data structure there. Now, if we need to retrieve value object in this situation, following steps will be followed :
+Things get little tricky, when Interviewer ask second question, What happens if two keys has same hashCode?
+If multiple keys has same hashCode, than during put() operation collision had occurred,
+which means multiple Entry object stored in a bucket location. 
+Each Entry keep track of another Entry, forming a linked list data structure there. 
+Now, if we need to retrieve value object in this situation, following steps will be followed :
 
 1) Call hashCode() method of key to find bucket location.
 
